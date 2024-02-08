210426387f6b869db837fcd3fc877a383ff3bf22
Djkarmur <dipakkarmur45@gmail.com>
2024-02-08 14:42:02 +0530

    authenticate file

---
+++ assets/js/authentication.js
@@ -0,0 +1,9 @@
+
+const loginFormData = document.getElementById("loginForm");
+
+loginFormData.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    const email = document.getElementById('emailId').value;
+    const passWord = document.getElementById('pwd').value;
+    
+})
